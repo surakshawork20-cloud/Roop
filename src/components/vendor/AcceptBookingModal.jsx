5972546506c90f69d773d172f40b795e3dee4121
shanmukh poladi <shanmukh@shanmukhs-MacBook-Air.local>
2026-03-18 19:23:05 +0530

    fix loader import case

--- src/components/vendor/AcceptBookingModal.jsx
+++ src/components/vendor/AcceptBookingModal.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
-import Loader from "@/components/loader";
+import Loader from "@/components/Loader";
 
 export default function AcceptBookingModal({
   booking,
